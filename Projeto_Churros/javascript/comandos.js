e42c4c03857ddf6690ee6829b634bc2d477e39bd
Matheus Bomfim <49158703+bomfim1209@users.noreply.github.com>
2023-11-07 21:29:29 -0300

    Add files via upload

--- Projeto_Churros/javascript/comandos.js
+++ Projeto_Churros/javascript/comandos.js
@@ -1,61 +1,116 @@
+///////// Validação tela de login /////////
 function validacao(){
-    var nome = document.getElementById("");
-    if(nome.value == ""){
-        alert("Nome não informado");
-        nome.focus();
+    var email = document.getElementById("email-validacao");
+    if(email.value == !email){
+        alert("Usuario não informado");
+        email.focus();
+    }
+
+    var senha = document.getElementById("senha-validacao");
+    if(senha.value == !senha){
+        alert("Senha não informada")
+        senha.focus();
     }
 }
 
 
-const mode = document.getElementById('variavel do icone');
+///////// Modo Dark /////////
+const mode = document.getElementById("icone-lua");
 
-mode.addEventListener('click', ()=> {
-    const form = document.getElementById('nome da classe');
-    if(mode.classList.contains('nome da classe do icone')){
-        mode.classList.remove('nome da classe do icone');
-        mode.classList.add('nome da classe do icone');
+mode.addEventListener("click", () => {
+    const form = document.getElementById("container-login");
 
-        form.classList.add('dark');
+    if(mode.classList.contains("fa-moon")){
+        mode.classList.remove("fa-moon");
+        mode.classList.add("fa-sun");
+
+        form.classList.add("dark");
         return;
     }
 
-    mode.classList.add('nome da classe do icone');
-    mode.classList.remove('nome da classe do icone');
-    form.classList.remove('dark');
+    mode.classList.add("fa-sun");
+    mode.classList.remove("fa-moon");
+    form.classList.remove("dark");
 });
 
 
 /////////////////////////////////////////////////////////////
+// Verificação do CEP
+function limpa_formulário_cep() {
+    //Limpa valores do formulário de cep.
+    document.getElementById('Rua/Avenida').value=("");
+    document.getElementById('Bairro').value=("");
+    document.getElementById('Cidade').value=("");
+    document.getElementById('Estado').value=("");
+}
 
-(function(){ 
- 
-    const cep = document.querySelector("input[name=cep]");
- 
-    cep.addEventListener('blur', e=> {
-        const value = cep.value.replace(/[^0-9]+/, '');
-        const url = `https://viacep.com.br/ws/${value}/json/`;
- 
-        fetch(url)
-        .then( response => response.json())
-        .then( json => {
-            if( json.logradouro ) {
-                document.querySelector('input[name=rua]').value = json.logradouro;
-                document.querySelector('input[name=bairro]').value = json.bairro;
-                document.querySelector('input[name=cidade]').value = json.localidade;
-                document.querySelector('input[name=estado]').value = json.uf;
-            }
-        });
-    });
- 
-})();
+function meu_callback(conteudo) {
+if (!("erro" in conteudo)) {
+    //Atualiza os campos com os valores.
+    document.getElementById('Rua/Avenida').value=(conteudo.logradouro);
+    document.getElementById('Bairro').value=(conteudo.bairro);
+    document.getElementById('Cidade').value=(conteudo.localidade);
+    document.getElementById('Estado').value=(conteudo.uf);
+} //end if.
+else {
+    //CEP não Encontrado.
+    limpa_formulário_cep();
+    alert("CEP não encontrado.");
+}
+}
+
+function pesquisacep(valor) {
+
+//Nova variável "cep" somente com dígitos.
+var cep = document.getElementById("cep");
+
+//Verifica se campo cep possui valor informado.
+if (cep != "") {
+
+    //Expressão regular para validar o CEP.
+    var validacep = /^[0-9]{8}$/;
+
+    //Valida o formato do CEP.
+    if(validacep.test(cep)) {
+
+        //Preenche os campos com "..." enquanto consulta webservice.
+        document.getElementById('Rua/Avenida').value="...";
+        document.getElementById('Bairro').value="...";
+        document.getElementById('Cidade').value="...";
+        document.getElementById('Estado').value="...";
+
+        //Cria um elemento javascript.
+        var script = document.createElement('script');
+
+        //Sincroniza com o callback.
+        script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+        //Insere script no documento e carrega o conteúdo.
+        document.body.appendChild(script);
+
+    } //end if.
+    else {
+        //cep é inválido.
+        limpa_formulário_cep();
+        alert("Formato de CEP inválido.");
+    }
+} //end if.
+else {
+    //cep sem valor, limpa formulário.
+    limpa_formulário_cep();
+}
+};
+/////////////////////////////////////////////////////////////
 
 
-
+// Validar E-mail
 function validateEmailAddress(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 }
 
+
+// Validação da data
 function validate() {
     $("#result").text("");
     var emailaddress = $("#email").val();
