34fd5105f2882a9b77c28ad58f805efe29e7523c
Matheus Bomfim <49158703+bomfim1209@users.noreply.github.com>
2023-11-20 13:21:46 -0300

    Add files via upload
    Atualização das paginas de login, cadastro e seus respectivos CSS.

--- Projeto_Churros/javascript/comandos.js
+++ Projeto_Churros/javascript/comandos.js
@@ -14,6 +14,19 @@ function validacao(){
 }
 
 
+///////// Menu responsivo /////////
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "../imagens/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "../imagens/close_white_36dp.svg";
+    }
+}
+
+
 ///////// Modo Dark /////////
 const mode = document.getElementById("icone-lua");
 
