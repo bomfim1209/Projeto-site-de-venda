9c8cddefe76f2947b289bc0ad78733635c0b76c3
Matheus Bomfim <49158703+bomfim1209@users.noreply.github.com>
2023-11-25 19:39:04 -0300

    Add files via upload
    Atualização do cadastro e JS.

--- Projeto_Churros/javascript/comandos.js
+++ Projeto_Churros/javascript/comandos.js
@@ -27,7 +27,6 @@ function menuShow() {
 }
 
 ///////// Modo Dark /////////
-///////// Modo Dark Login /////////
 const login = document.getElementById("item-lua");
 
 login.addEventListener("click", () => {
@@ -46,10 +45,76 @@ login.addEventListener("click", () => {
 
     form.classList.remove("dark");
 });
-
-
 /////////////////////////////////////////////////////////////
-// Verificação do CEP
+
+
+/////////// TELA DE CADASTRO ///////////
+function validacao_cadastro(){
+    
+    ////////// VALIDAR SENHA //////////
+    senha = document.getElementById("senha").value;
+    conf_senha = document.getElementById("conf_enha").value;
+
+    if (senha != conf_senha) {
+        senhaC.setCustomValidity("Senhas diferentes!");
+        return false;
+    } else {
+        return true;
+    }
+    
+    
+    
+    
+    
+    const cpf = document.getElementById("cpf");
+    validacaoCPF();
+
+    /////////// CPF ///////////
+    function validarCPF(cpf) {	
+        cpf = cpf.replace(/[^\d]+/g,'');	
+        if(cpf == '') return false;	
+        // Elimina CPFs invalidos conhecidos	
+        if (cpf.length != 11 || 
+            cpf == "00000000000" || 
+            cpf == "11111111111" || 
+            cpf == "22222222222" || 
+            cpf == "33333333333" || 
+            cpf == "44444444444" || 
+            cpf == "55555555555" || 
+            cpf == "66666666666" || 
+            cpf == "77777777777" || 
+            cpf == "88888888888" || 
+            cpf == "99999999999")
+                return false;
+
+        // Valida 1o digito	
+        add = 0;	
+        for (i=0; i < 9; i ++)		
+            add += parseInt(cpf.charAt(i)) * (10 - i);	
+            rev = 11 - (add % 11);	
+            if (rev == 10 || rev == 11)		
+                rev = 0;	
+            if (rev != parseInt(cpf.charAt(9)))		
+                return false;
+
+        // Valida 2o digito	
+        add = 0;	
+        for (i = 0; i < 10; i ++)		
+            add += parseInt(cpf.charAt(i)) * (11 - i);	
+        rev = 11 - (add % 11);	
+        if (rev == 10 || rev == 11)	
+            rev = 0;	
+        if (rev != parseInt(cpf.charAt(10)))
+            return false;		
+        return true;   
+    }
+}
+
+
+
+
+
+/////////// Verificação do CEP ///////////
 function limpa_formulário_cep() {
     //Limpa valores do formulário de cep.
     document.getElementById('Rua/Avenida').value=("");
@@ -74,45 +139,44 @@ function meu_callback(conteudo) {
 }
 
 function pesquisacep(valor) {
+    //Nova variável "cep" somente com dígitos.
+    var cep = document.getElementById("cep");
 
-//Nova variável "cep" somente com dígitos.
-var cep = document.getElementById("cep");
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
 
-//Verifica se campo cep possui valor informado.
-if (cep != "") {
+        //Expressão regular para validar o CEP.
+        var validacep = /^[0-9]{8}$/;
 
-    //Expressão regular para validar o CEP.
-    var validacep = /^[0-9]{8}$/;
+        //Valida o formato do CEP.
+        if(validacep.test(cep)) {
 
-    //Valida o formato do CEP.
-    if(validacep.test(cep)) {
+            //Preenche os campos com "..." enquanto consulta webservice.
+            document.getElementById('Rua/Avenida').value="...";
+            document.getElementById('Bairro').value="...";
+            document.getElementById('Cidade').value="...";
+            document.getElementById('Estado').value="...";
 
-        //Preenche os campos com "..." enquanto consulta webservice.
-        document.getElementById('Rua/Avenida').value="...";
-        document.getElementById('Bairro').value="...";
-        document.getElementById('Cidade').value="...";
-        document.getElementById('Estado').value="...";
+            //Cria um elemento javascript.
+            var script = document.createElement('script');
 
-        //Cria um elemento javascript.
-        var script = document.createElement('script');
+            //Sincroniza com o callback.
+            script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
 
-        //Sincroniza com o callback.
-        script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+            //Insere script no documento e carrega o conteúdo.
+            document.body.appendChild(script);
 
-        //Insere script no documento e carrega o conteúdo.
-        document.body.appendChild(script);
-
+        } //end if.
+        else {
+            //cep é inválido.
+            limpa_formulário_cep();
+            alert("Formato de CEP inválido.");
+        }
     } //end if.
     else {
-        //cep é inválido.
+        //cep sem valor, limpa formulário.
         limpa_formulário_cep();
-        alert("Formato de CEP inválido.");
     }
-} //end if.
-else {
-    //cep sem valor, limpa formulário.
-    limpa_formulário_cep();
-}
 };
 /////////////////////////////////////////////////////////////
 
@@ -138,5 +202,3 @@ function validate() {
     return false;
 }
 $("#validate").bind("click", validate);
-
-// BOTÃO PARA TOPO D PAGINA
